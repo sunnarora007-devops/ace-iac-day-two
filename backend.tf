87359aeaf5ddc70262924cc7cea4565fccda4a4f
sunnarora007-devops <106085790+sunnarora007-devops@users.noreply.github.com>
2022-05-25 15:16:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-     organization = "BT-Azure"
+    organization = "BT-Azure"
     workspaces {
       name = "ace-iac-day-two"
     }
